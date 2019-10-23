28104fb2c622b0bc2f97b77848bc60661a2e9519
William Oropallo <woropallo@gmail.com>
2019-10-23 13:45:02 -0400

    Added public init to Card

--- Sources/BitCardPoker/BCCard.swift
+++ Sources/BitCardPoker/BCCard.swift
@@ -1,4 +1,9 @@
 public struct BCCard: Equatable {
     public let rank: BCRank
     public let suit: BCSuit
+    
+    public init(rank: BCRank, suit: BCSuit) {
+        self.rank = rank
+        self.suit = suit
+    }
 }
